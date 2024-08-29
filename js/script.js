48c7daa8cd94abe90a772703cc5910d1a13c8001
kawsar002 <kawsarhossain.tipu@gmail.com>
2024-08-29 14:58:36 +0600

    portfolio

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var typed= new Typed(".typing",{
+    strings: ["Web Designer", "Web Developer", "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+})
+
+// var typed = new Typed(".typing" , {
+//     strings: ["Designer","Developer","FullStack"],
+//     typeSpeed:100,
+//     backSpeed: 100,
+//     loop: true,
+// });
